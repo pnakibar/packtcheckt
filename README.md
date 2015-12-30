eecc8a02c1418649d3c2b93a5810755aa2019076
pnakibar <pedro.nakibar@xdevel.com.br>
2015-12-30 11:43:15 -0200

    caching added

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # README
-
 This script check which is the free book of the day in the Packt Publishing.
 
 Everyday there are new books, and they are usually good books, so it's good to keep and eye in it. This script is to automate it!
--- packtcheckt.py
+++ packtcheckt.py
@@ -1,16 +1,65 @@
 #!/usr/bin/python
 import urllib2
 import re
+from sys import argv
+import datetime
 
 
-response = urllib2.urlopen('https://www.packtpub.com/packt/offers/free-learning')
-html = response.read()
+cachePath = "/tmp/packtchecktcache"
+def getToday():
+	now = datetime.datetime.now()
+	return now.day
 
-bookNameRegex = re.compile(ur'<div class="dotd-title">\s*<h2>\s*(.*)\s*<\/h2>\s*<\/div>')
+def isToday(dayToBeTested):
+	today = getToday()
+	return dayToBeTested == str(today)
+	
 
-bookName = re.search(bookNameRegex, html).group(1)
+def getBookNameFromCache():
+	try: 
+		f = open(cachePath, 'r')
+		today, bookName = f.readlines()
+		today = today.strip()
+		bookName = bookName.strip()
 
-print("The free book of the day is:\n"+bookName+"\n\nMore information at: https://www.packtpub.com/packt/offers/free-learning")
+		if (isToday(today)):
+			return bookName
+		else:
+			return None
+	except Exception:
+		return None
+
+		
+
+def writeToCache(bookName):
+	f = open(cachePath, 'w')
+	
+	day = getToday()
+
+	f.write(str(day))
+	f.write("\n")
+	f.write(bookName)
+
+	f.close()
+
+
+bookName = getBookNameFromCache()
+
+if not bookName:
+	print("Retrieving the new book...")
+
+	response = urllib2.urlopen('https://www.packtpub.com/packt/offers/free-learning')
+	html = response.read()
+
+	bookNameRegex = re.compile(ur'<div class="dotd-title">\s*<h2>\s*(.*)\s*<\/h2>\s*<\/div>')
+
+	bookName = re.search(bookNameRegex, html).group(1)
+
+	writeToCache(bookName)
+
+print("The free book of the day is:")
+print(bookName)
+print("\nMore information at: https://www.packtpub.com/packt/offers/free-learning")
 
 
 
